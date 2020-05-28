61ff0327a06a22cae59c61c65c084cb4d687b688
vutrungnghia <nghia.vt173284@sis.hust.edu.vn>
2020-05-28 09:46:56 +0700

    choose-pair-up-low-iou-and-choose-2048-keypoints-with-max-scores

--- process_data.py
+++ process_data.py
@@ -22,8 +22,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--input', type=str, default='/media/vutrungnghia/New Volume/P2-ImageMatchingChallenge/dataset/superglue/train/input')
 parser.add_argument('--output', type=str, default='/media/vutrungnghia/New Volume/P2-ImageMatchingChallenge/dataset/superglue/train/output')
 parser.add_argument('--scene', type=str, default='reichstag')
-parser.add_argument('--iou_thresold', type=float, default=0.1)
-parser.add_argument('--max_pairs', type=int, default=10000)
+parser.add_argument('--up_iou_thresold', type=float, default=0.1)
+parser.add_argument('--low_iou_thresold', type=float, default=0.05)
 args = parser.parse_args()
 
 logging.info('\n======== SUPERGLUE - DATA PREPROCESSING ========\n')
@@ -55,20 +55,18 @@ for point_id in tqdm(points.keys()):
             all_pairs[pair] = {'intersection': 0}
         all_pairs[pair]['intersection'] += 1
 
-random_pair = {0}
-n = min(args.max_pairs, int(len(all_pairs)/10))  # maximum number of pairs to be token from all pairs
-while len(random_pair) < n:
-    random_index = np.random.randint(0, len(all_pairs))
-    random_pair.add(random_index)
-random_pair = list(random_pair)
+for (u, v) in all_pairs.keys():
+    u_kpts = local_features[u]['xys'].shape[0]
+    v_kpts = local_features[v]['xys'].shape[0]
+    all_pairs[(u, v)]['union'] = u_kpts + v_kpts - all_pairs[(u, v)]['intersection']
+    all_pairs[(u, v)]['iou'] = all_pairs[(u, v)]['intersection'] / all_pairs[(u, v)]['union']
 
 dataset = {}
-keys = list(all_pairs.keys())
-for index in random_pair:
-    dataset[keys[index]] = {'matches': []}
+for k, v in all_pairs.items():
+    if v['iou'] > args.low_iou_thresold and v['iou'] < args.up_iou_thresold:
+        dataset[k] = {'matches': []}
 
 logging.info(f'No.pair filter/all: {len(dataset)}/{len(all_pairs)}')
-
 # generate list of matched index keypoints for each pairs after filtering
 for point_id in tqdm(points.keys()):
     point = points[point_id]
@@ -138,82 +136,77 @@ np.save(OUTPUT, dataset)
 logging.info(f'Saved data at: {OUTPUT}')
 logging.info('Completed\n')
 
-# # pair = dataset[(1, 2)]
-# # this function is used to create groundtruth, filtered keypoints, descriptors and scores on the fly during training
-# def transform_and_filter(pair: dict, max_keypoints=2048) -> dict:
-#     """get groundtruth matrix for a pair of images
-#     Arguments:
-#         pair {dict}:
-#             matches: [(idx1, idx2), .... ]
-#             keypoints: (N x 2, M x 2)
-#             descriptors: (N x 128, M x 128)
-#             scores: (N, M)
-#             3dpoints: (N, M)
-#             shape: ({width: , height: }, {width: , height: })
-#             name: (str, str)
-#     Returns:
-#         np.array -- [description]
-#     """
-#     assert pair['keypoints'][0].shape[0] == pair['descriptors'][0].shape[0]
-#     assert pair['keypoints'][0].shape[0] == pair['scores'][0].shape[0]
-#     assert pair['keypoints'][0].shape[0] == pair['3dpoints'][0].shape[0]
+# pair = dataset[(6, 42)]
+# this function is used to create groundtruth, filtered keypoints, descriptors and scores on the fly during training
+def transform_and_filter(pair: dict, top_k=2048) -> dict:
+    """get groundtruth matrix for a pair of images
+    Arguments:
+        pair {dict}:
+            matches: [(idx1, idx2), .... ]
+            keypoints: (N x 2, M x 2)
+            descriptors: (N x 128, M x 128)
+            scores: (N, M)
+            3dpoints: (N, M)
+            shape: ({width: , height: }, {width: , height: })
+            name: (str, str)
+    Returns:
+        np.array -- [description]
+    """
+    assert pair['keypoints'][0].shape[0] == pair['descriptors'][0].shape[0]
+    assert pair['keypoints'][0].shape[0] == pair['scores'][0].shape[0]
+    assert pair['keypoints'][0].shape[0] == pair['3dpoints'][0].shape[0]
 
-#     assert pair['keypoints'][1].shape[0] == pair['descriptors'][1].shape[0]
-#     assert pair['keypoints'][1].shape[0] == pair['scores'][1].shape[0]
-#     assert pair['keypoints'][1].shape[0] == pair['3dpoints'][1].shape[0]
+    assert pair['keypoints'][1].shape[0] == pair['descriptors'][1].shape[0]
+    assert pair['keypoints'][1].shape[0] == pair['scores'][1].shape[0]
+    assert pair['keypoints'][1].shape[0] == pair['3dpoints'][1].shape[0]
 
-#     # get keypoints, descriptors and scores base on filter of has 3d points and descriptors
-#     has_3d_points0 = pair['3dpoints'][0] != -1
-#     has_descriptors0 = ~np.isnan(pair['descriptors'][0][:, 0])
-#     filter0 = has_3d_points0 * has_descriptors0
-#     if sum(filter0) > max_keypoints:
-#         true_idxs = np.argwhere(filter0 == True).squeeze()
-#         np.random.shuffle(true_idxs)
-#         reduced_true_idxs = true_idxs[:max_keypoints]
-#         filter0 = filter0 * False
-#         filter0[reduced_true_idxs] = True
-#     keypoints0 = pair['keypoints'][0][filter0]
-#     descriptors0 = pair['descriptors'][0][filter0]
-#     scores0 = pair['scores'][0][filter0]
+    # get keypoints, descriptors and scores base on filter of has 3d points and descriptors
+    # has_3d_points0 = pair['3dpoints'][0] != -1
+    has_descriptors0 = ~np.isnan(pair['descriptors'][0][:, 0])
+    top_k_scores0_idxs = np.argsort(
+        -(pair['scores'][0] + has_descriptors0))[:top_k]
+    top_k_filter0 = np.array([False] * has_descriptors0.shape[0])
+    top_k_filter0[top_k_scores0_idxs] = True
+    filter0 = top_k_filter0 * has_descriptors0
+    keypoints0 = pair['keypoints'][0][filter0]
+    descriptors0 = pair['descriptors'][0][filter0]
+    scores0 = pair['scores'][0][filter0]
 
-#     has_3d_points1 = pair['3dpoints'][1] != -1
-#     has_descriptors1 = ~np.isnan(pair['descriptors'][1][:, 0])
-#     filter1 = has_3d_points1 * has_descriptors1
-#     if sum(filter1) > max_keypoints:
-#         true_idxs = np.argwhere(filter1 == True).squeeze()
-#         np.random.shuffle(true_idxs)
-#         reduced_true_idxs = true_idxs[:max_keypoints]
-#         filter1 = filter1 * False
-#         filter1[reduced_true_idxs] = True
-#     keypoints1 = pair['keypoints'][1][filter1]
-#     descriptors1 = pair['descriptors'][1][filter1]
-#     scores1 = pair['scores'][1][filter1]
+    has_descriptors1 = ~np.isnan(pair['descriptors'][1][:, 0])
+    top_k_scores1_idxs = np.argsort(
+        -(pair['scores'][1] + has_descriptors1))[:top_k]
+    top_k_filter1 = np.array([False] * has_descriptors1.shape[0])
+    top_k_filter1[top_k_scores1_idxs] = True
+    filter1 = top_k_filter1 * has_descriptors1
+    keypoints1 = pair['keypoints'][1][filter1]
+    descriptors1 = pair['descriptors'][1][filter1]
+    scores1 = pair['scores'][1][filter1]
 
-#     # construct the groundtruth matrix for keypoints after filtering
-#     height, width = pair['keypoints'][0].shape[0] + 1, pair['keypoints'][1].shape[0] + 1
-#     groundtruth = np.zeros((height, width))
-#     for kp1_idx, kp2_idx in pair['matches']:
-#         groundtruth[kp1_idx][kp2_idx] = 1
+    # construct the groundtruth matrix for keypoints after filtering
+    height, width = pair['keypoints'][0].shape[0] + 1, pair['keypoints'][1].shape[0] + 1
+    groundtruth = np.zeros((height, width))
+    for kp1_idx, kp2_idx in pair['matches']:
+        groundtruth[kp1_idx][kp2_idx] = 1
 
-#     filter0 = np.append(filter0, True)
-#     filter1 = np.append(filter1, True)
-#     groundtruth = groundtruth[filter0, :]
-#     groundtruth = groundtruth[:, filter1]
+    filter0 = np.append(filter0, True)
+    filter1 = np.append(filter1, True)
+    groundtruth = groundtruth[filter0, :]
+    groundtruth = groundtruth[:, filter1]
 
-#     right_append = groundtruth.sum(axis=1) == 0
-#     bottom_append = groundtruth.sum(axis=0) == 0
-#     groundtruth[:, -1] += right_append
-#     groundtruth[-1, :] += bottom_append
-#     groundtruth[-1][-1] = 0
+    right_append = groundtruth.sum(axis=1) == 0
+    bottom_append = groundtruth.sum(axis=0) == 0
+    groundtruth[:, -1] += right_append
+    groundtruth[-1, :] += bottom_append
+    groundtruth[-1][-1] = 0
 
-#     return {
-#         'keypoints': (keypoints0, keypoints1),
-#         'descriptors': (descriptors0.transpose(), descriptors1.transpose()),  # N x 128 => 128 x N, M x 128 => 128 x M
-#         'scores': (scores0, scores1),
-#         'shape': pair['shape'],
-#         'name': pair['name'],
-#         'groundtruth': groundtruth
-#     }
+    return {
+        'keypoints': (keypoints0, keypoints1),
+        'descriptors': (descriptors0.transpose(), descriptors1.transpose()),  # N x 128 => 128 x N, M x 128 => 128 x M
+        'scores': (scores0, scores1),
+        'shape': pair['shape'],
+        'name': pair['name'],
+        'groundtruth': groundtruth
+    }
 
 # import matplotlib.pyplot as plt
 # s = transform_and_filter(dataset[list(dataset.keys())[0]])
